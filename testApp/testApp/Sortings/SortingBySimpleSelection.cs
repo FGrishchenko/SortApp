5e3492c921a76621590665f791c741366b101874
Grishchenko, Fyodor <Fyodor.Grishchenko@colliers.com>
2022-03-22 01:39:42 +0400

    Create three sorting methods and utilities for them.

---
+++ testApp/testApp/Sortings/SortingBySimpleSelection.cs
@@ -0,0 +1,23 @@
+﻿namespace testApp.Sortings
+{
+    public class SortingBySimpleSelection
+    {
+        public void SortArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[min])
+                    {
+                        min = j;
+                    }
+                }
+                int temp = arr[min];
+                arr[min] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+}
---
+++ testApp/testApp/Utilitites/TimeUtils.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+namespace testApp.Utilitites
+{
+    public static class TimeUtils
+    {
+        public static string SortAndGetTime(Action doSomething, Stopwatch StartTime)
+        {
+            StartTime.Restart();
+            StartTime.Start();
+            doSomething.Invoke();
+            StartTime.Stop();
+            return "Run time --> " + StartTime.Elapsed;
+        }
+    }
+}
